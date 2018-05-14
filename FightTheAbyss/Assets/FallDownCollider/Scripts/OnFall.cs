73f7e017a508d64cce3950fa0aa353c396aa8667
Unknown <pedrorivero19@gmail.com>
2018-05-14 20:22:35 +0200

    Respawns si caes
    MATADME

---
+++ FightTheAbyss/Assets/FallDownCollider/Scripts/OnFall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FightTheAbyss
+{
+    public class OnFall : MonoBehaviour
+    {
+
+        public GameObject player;
+        public Transform respawnPoint;
+
+        public void changeRespawnPoint(Transform newRespawnPoint)
+        {
+            respawnPoint = newRespawnPoint;
+        }
+
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.CompareTag("Player"))
+            {
+                col.gameObject.transform.position = respawnPoint.position;
+                col.gameObject.GetComponent<States>().health -= 10;
+            }
+            else if (col.CompareTag("Enemy"))
+            {
+                Destroy(col.gameObject);
+            }
+        }
+    }
+}
